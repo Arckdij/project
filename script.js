b4993786b35cf3f8c396ac2e7fd8b402f82f2038
Adik <telmana26@yandex.ru>
2019-12-12 15:07:12 +0300

    firs commit

---
+++ script.js
@@ -0,0 +1,16 @@
+"use strict"
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMaried: false
+};
+console.log(persone.name);
+
+let arr = ["plump.png", "orange.pjpg", "apple.bmp"];
+
+console.log(arr[1]);
+
+alertt("Hello");
+
+confirm()
